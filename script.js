81581284cb259539083adf5cda06c6d168218781
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-10-28 10:42:47 +0100

    2_1.1

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 (function () {
-  const example = document.getElementById('example')
-  const cw1 = document.getElementById('cw1')
-  const cw2 = document.getElementById('cw2')
-  const cw3 = document.getElementById('cw3')
-  const answer = document.getElementById('answer')
+  const example = document.getElementById("example");
+  const cw1 = document.getElementById("cw1");
+  const cw2 = document.getElementById("cw2");
+  const cw3 = document.getElementById("cw3");
+  const answer = document.getElementById("answer");
 
   example.addEventListener("click", function () {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(array => {
-        console.log(array)
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((array) => {
+        console.log(array);
         answer.innerHTML = JSON.stringify(array);
-      })
-  })
+      });
+  });
 
   cw1.addEventListener("click", function () {
-    //TODO
-  })
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((posts) => {
+        let html = "<ul>";
+
+        posts.forEach((post) => {
+          html += `
+          <li>
+            <strong>${post.title}</strong><br>
+            <em>${post.body}</em>
+          </li>
+        `;
+        });
+
+        html += "</ul>";
+
+        answer.innerHTML = html;
+      })
+      .catch(err);
+  });
 
   cw2.addEventListener("click", function () {
     //TODO
-  })
+  });
 
   cw3.addEventListener("click", function () {
     //TODO
-  })
-
+  });
 })();
